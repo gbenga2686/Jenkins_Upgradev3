d5ce80b6770661c7179910990f862e5b4df6c598
Gbenga <gbenga.olusoto@gmail.com>
2021-05-18 17:35:48 +0100

    changing test code

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+
+    @Test
+  public void greetShouldIncludeGreetingMessages() {
+    String someone = "World, Thanks you making me sweet";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
